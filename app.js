d93f8775ba80e787280b9f453ab3dfd4e915f81e
AmitSahar16 <amitsahar16@gmail.com>
2026-01-22 15:25:52 +0200

    feat(main): add base express app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+async function startServer() {
+  const app = express();
+  const port = process.env.PORT || 4000;
+
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+  app.use(bodyParser.json());
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+  module.exports = app;
+}
+
+startServer();
+
